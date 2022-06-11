805ec668f6c2a19276ce93f94d8e9bcc71796817
Sapfire65 <cot65@yandex.ru>
2022-06-11 16:44:47 +0300

    Тестовые сценарии №2

--- test_abs_project.py
+++ test_abs_project.py
@@ -1,10 +1,11 @@
 def test_abs1():
     assert abs(-42) == 42, "Should be absolute value of a number"
 
-# Для подтверждения того, что данный скрипт был запущен напрямую,
-# а не вызван внутри другого файла в качестве модуля. Весь код написанный
-# в теле этого условия будет выполнен только если пользователь запустил файл самостоятельно.
+def test_abs2():
+    assert abs(-42) == -42, "Should be absolute value of a number"
+
 if __name__ == "__main__":
     test_abs1()
-    print("All tests passed!")
+    test_abs2()
+    print("Everything passed")
 
