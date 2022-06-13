84e5d5fea9fd867b68b096ffda56ced7f7e3c090
Sapfire65 <cot65@yandex.ru>
2022-06-13 09:05:43 +0300

    Sewed requirements

--- lession/test_fixture1.py
+++ lession/test_fixture1.py
@@ -1,27 +1,28 @@
 import pytest
+import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
+link = "https://stepik.org/"
 
-
-@pytest.fixture
-# Фикстура каждый раз будет выводиь сообщение и запускать новое окно браузера.
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    return browser
-
+@pytest.fixture()
+def target_stepik():
+    print('\nЭто сообщение повторяется каждый раз, после запуска фикстуры')
+    target_stepik = webdriver.Chrome()
+    yield target_stepik
+    print('<< Браузер закрыт')
+    time.sleep(5)
+    target_stepik.quit()
 
 class TestMainPage1():
-    # вызываем фикстуру в тесте, передав ее как параметр
-    def test_guest_should_see_login_link(self, browser):
-        browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, "#login_link")
+    # вызываем фикстуру в тесте, передав ее как параметр 'target_stepik'
+    def test_number_1(self, target_stepik):
+        target_stepik.get(link)
+        target_stepik.find_element(By.CSS_SELECTOR, '[class="navbar"] [aria-label="Search"]').send_keys('Автоматизация Selenium')
 
-    def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-        browser.get(link)
-        browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
+    def test_number_2(self, target_stepik):
+        target_stepik.get(link)
+        target_stepik.find_element(By.CSS_SELECTOR, '[id="ember248"]').click()
 
 
 
---
+++ my_experements/conftest.py
@@ -0,0 +1,25 @@
+# import pytest
+# from selenium import webdriver
+#
+# @pytest.fixture()
+#
+# def poisk():
+#     print('\nЭто ТОП писковик, проходит проверку на доступность')
+#     poisk = webdriver.Chrome()
+#     return poisk
+
+
+
+    # print('Введите свой логин:', end='  ')
+    # login = input()
+    # print('Введите свой пароль:', end='  ')
+    # password = input()
+    #
+    # if len(login) == 0 or len(password) ==0:
+    #     print('Поля логин и пароль обязательны к заполнению!', end='\n\n')
+    #     input_login_password()
+
+
+
+
+
