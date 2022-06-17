270275f13eae6dec18647231a85e1d9515e6ffaf
Sapfire65 <cot65@yandex.ru>
2022-06-18 00:56:59 +0300

    update 18.06

--- linkedin_auto/test_linkedin_frends.py
+++ linkedin_auto/test_linkedin_frends.py
@@ -6,22 +6,33 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 
+link = 'https://www.linkedin.com/'
+login = 'cherenkova-iylia@mail.ru'
+password = 'Iy1ia123'
 
+login1 = 'sapfiretrey@gmail.com'
+password1 = '!K153ON34rus!'
 
 try:
     def test_cklick_button_accept_quke(autch):
-        # Открываю ссылку в браузере | нахожу кнопку | кликаю по кнопке согласия
+        # Открываю ссылку в браузере
         autch.get(link)
-        autch.find_element(By.CSS_SELECTOR, '[action-type="ACCEPT"]').click()
+        try:
+            #  | нахожу кнопку | кликаю по кнопке согласия
+            button_accept = WebDriverWait(autch, 60).until(EC.element_to_be_clickable((By.XPATH, '[action-type="ACCEPT"]')))
+        except:
+            pass
 
-        autch.find_element(By.XPATH, '//input[@autocomplete="username"]').send_keys(login)
-        autch.find_element(By.XPATH, '//input[@autocomplete="current-password"]').send_keys(password)
+        autch.find_element(By.XPATH, '//input[@autocomplete="username"]').send_keys(login1)
+        autch.find_element(By.XPATH, '//input[@autocomplete="current-password"]').send_keys(password1)
         autch.find_element(By.XPATH, '//button[@class="sign-in-form__submit-button"]').click()
 
-        # Явное ожидание элемента после авторизации.
-        button_chat = WebDriverWait(autch, 60).until(EC.element_to_be_clickable((By.XPATH, '//li-icon[@class="artdeco-button__icon"][@type="chevron-down-icon"]')))
-        time.sleep(1)
+
+        # Явное ожидание элемента после авторизации. Свернуть чат.
+        button_chat = WebDriverWait(autch, 120).until(EC.element_to_be_clickable(
+            (By.XPATH, '(//div[@class="msg-overlay-bubble-header__controls display-flex"]/button)[2]')))
         button_chat.click()
+
 
         # находим раздел с контактами
         autch.find_element(By.XPATH, '(//nav[@class="global-nav__nav"]//li)[2]').click()
@@ -52,12 +63,12 @@ try:
 
             # Скролим страницу пока не будет видно все контакты.
             # Получаю количество элементов c линками на профили.
-            # Условие цикла: скролить вниз, пока на странице не будет всего списка контактов.
+            # Условие цикла: скролить вниз, пока на странице не перестанет обнаруживатся кнопака в футере.
 
             # Пока список не кончится.
             amount_elements = 0
 
-            for i in range(1, friends_list + 1):
+            for i in range(0, friends_list + 1):
                 # Проверка наличия следующего элемента. Если он есть, скролить и проверять на наличие кнопки в футере списка.
                 # Получаю первый элемент
                 amount_elements = len(autch.find_elements_by_xpath(f'(//a[@class="ember-view mn-connection-card__picture"])[{i}]'))
@@ -66,15 +77,15 @@ try:
                 if amount_elements == 1:
                     autch.execute_script('window.scrollBy(0, 50);')
                     try:
-                        button_load_more = WebDriverWait(autch, 10).until(
+                        button_load_more = WebDriverWait(autch, 5).until(
                                 EC.element_to_be_clickable((By.XPATH, '//div[@class="display-flex p5"]')))
                         button_load_more.click()
                     except:
                         break
 
-            # Получаем общее количество найденных объектов
+            # Получаем общее количество найденых объектов
             amount_elements = len(autch.find_elements_by_xpath('(//a[@class="ember-view mn-connection-card__picture"])'))
-            print(f'Найденно [ {amount_elements} ] контактов.')
+            print(f'Найденно {amount_elements} контактов.')
 
             # Получаем ссылку на профиль и добавляем в файл db_frends.txt
             for i in range(1, amount_elements + 1):
@@ -83,9 +94,11 @@ try:
                         (By.XPATH, f'(//a[@class="ember-view mn-connection-card__picture"])[{i}]'))).get_attribute('href')
 
                 db_frends = open("db_frends.txt", "a+")
-                db_frends.write(f'\n{friends_link}')
+                db_frends.write(f'{friends_link}\n')
                 db_frends.close()
             print('*' * 30, end='\n\n')
+
+
 
 
 
