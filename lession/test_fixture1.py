8798c736772aba00e834f41e5cd897e339a14af4
Sapfire65 <cot65@yandex.ru>
2022-06-22 17:17:04 +0300

    Задание: параметризация тестов

--- lession/test_fixture1.py
+++ lession/test_fixture1.py
@@ -1,42 +1,52 @@
 import pytest
+import math
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 
-link = "http://selenium1py.pythonanywhere.com/"
-link1 = 'http://ya.ru/'
-
-
-@pytest.fixture()
-def browser():
-    print("\nstart browser for test..")
-    browser = webdriver.Chrome()
-    yield browser
-    time.sleep(3)
-    print("\nquit browser..")
-    browser.quit()
 
 @pytest.fixture(scope='function')
-def print_messeg():
-    print('  << БАСНЯ')
-    print('Мартышка и очки')
+def browser():
 
-# class TestMainPage1():
+    print('\nНачало теста')
+    browser = webdriver.Chrome()
+    browser.implicitly_wait(10) # неявное ожидание
+
+    yield browser
+    full_text = ''
+    time.sleep(2)
+    browser.quit()
+
+@pytest.mark.parametrize('number_list', ['236895', '236896', '236897', '236898', '236899', '236903', '236904', '236905' ])
+def test_open_linc(browser, number_list):
+    answer = math.log(int(time.time()))
+    link = f'https://stepik.org/lesson/{number_list}/step/1'
+    browser.get(link)
+    browser.find_element(By.XPATH, '//textarea[@autocapitalize="none"]').send_keys(answer)
+    browser.find_element(By.XPATH, '//button[@class="submit-submission"]').click()
+    text = browser.find_element(By.XPATH, '//p[@class="smart-hints__hint"]').text
+    assert 'Correct!' == text, f'Ожидаемое значение <Correct!>, фактическое значение <<{text}>>'
+
+
+
+
+
+
+
+
+
+
+# @pytest.fixture(scope="function")
+# def browser():
+#     print("\nstart browser for test..")
+#     browser = webdriver.Chrome()
+#     yield browser
+#     print("\nquit browser..")
+#     browser.quit()
 #
-#     # вызываем фикстуру в тесте, передав ее как параметр
-#     def test_guest_should_see_login_link(self, browser):
-#         print("start test1")
-#         browser.get(link)
-#         browser.find_element(By.CSS_SELECTOR, "#login_link")
-#         print("finish test1")
-#
-#     def test_guest_should_see_basket_link_on_the_main_page(self, browser):
-#         print("start test2")
-#         browser.get(link)
-#         browser.find_element(By.CSS_SELECTOR, ".basket-mini .btn-group > a")
-#         print("finish test2")
+# @pytest.mark.parametrize('language', ["ru", "en-gb"])
+# def test_guest_should_see_login_link(browser, language):
+#     link = f"http://selenium1py.pythonanywhere.com/{language}/"
+#     browser.get(link)
+#     browser.find_element(By.CSS_SELECTOR, "#login_link")
 
-class TestMainPageYandex():
-
-    def test_super_text(self, print_messeg):
-        print('\nМартышка к старости слаба глазами стала .....\n')
